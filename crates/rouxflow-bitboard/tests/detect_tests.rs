83baf05f81a0c72368fd0ac5746540b132aa2015
DevOps Benjamin <devops.benjamin@gmail.com>
2026-02-15 20:04:16 +0100

    fxi test

--- crates/rouxflow-bitboard/tests/detect_tests.rs
+++ crates/rouxflow-bitboard/tests/detect_tests.rs
@@ -6,7 +6,7 @@ fn test_fb_block_detection() {
     assert!(cube.is_l_block_formed(), "Solved cube should have L-block");
     assert!(cube.is_fb_block(), "Solved cube should have blocks");
 
-    cube.rotate_y();
+    cube.rot_y();
     assert!(
         cube.is_l_block_formed(),
         "Rotated solved cube should have L-block"
@@ -14,14 +14,14 @@ fn test_fb_block_detection() {
     assert!(cube.is_fb_block(), "Rotated solved cube should have blocks");
 
     let mut cube = BitCube::new_solved();
-    cube.rotate_r2(); // Using the new explicit method
+    cube.face_r2(); // Using the new explicit method
     assert!(
         cube.is_l_block_formed(),
         "Rotating R2 shouldn't break L-block"
     );
     assert!(cube.is_fb_block());
 
-    cube.rotate_f();
+    cube.face_f();
     assert!(
         !cube.is_l_block_formed(),
         "Rotating F must break the specific L-block"
@@ -31,7 +31,7 @@ fn test_fb_block_detection() {
 #[test]
 fn test_pseudo_block_detection() {
     let mut cube = BitCube::new_solved();
-    cube.rotate_s();
+    cube.slice_s();
     assert!(!cube.is_l_block_formed(), "S move shuffles L face");
     // is_fb_block is still WIP for non-monochromatic pseudo-blocks if centers shifted
 }
